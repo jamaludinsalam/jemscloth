36d527bd575a96431e1b068071d48722bb5f678f
Jems.co <jamaludinsalam11@gmail.com>
2020-06-08 19:44:50 +0700

    mmoved our directory and shop data into its own reducer, created respective selectors and updated coresponding component with new flow form directory/shop

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,13 +7,21 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({collection}) => {
+const CollectionPage = ({ collection }) => {
     console.log(collection);
+    const { title, items } = collection;
     return(
+        <div className='collection-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+                {
+                    items.map(item => (
+                        <CollectionItem key={item.id} item={item}/>
+                    ))
+                }
+            </div>
+        </div>
 
-    <div className='collection-page'>
-    <h2>collection Page</h2>
-    </div>
 )}
 
 const mapStateToProps = (state, ownProps) => ({
